24f98f3df6d3975d07576122244badf255f81dae
Alex Doub <alexdoub1990@gmail.com>
2019-11-02 16:09:53 -0700

    IsNumber

---
+++ app/src/main/java/alex/com/challenges/IsNumber.kt
@@ -0,0 +1,43 @@
+package alex.com.challenges
+
+import java.lang.Exception
+import java.math.BigDecimal
+
+class IsNumber {
+    companion object {
+        fun isNumber(input: String) : Boolean {
+
+            var adjustedInput = input.trim()
+
+            //Simplify extremely large exponents
+            val indexOfE = adjustedInput.lastIndexOf("e")
+            if (indexOfE > 0 && indexOfE < adjustedInput.length - 1) {
+                var exponentString = adjustedInput.substring(indexOfE + 1)
+
+                // Check if exponent string is a real number
+                try {
+                    BigDecimal(exponentString)
+                } catch (e: Exception) {
+                    return false
+                }
+
+                // Fail if exponent is a decimal
+                if (exponentString.contains(".")) {
+                    return false
+                }
+
+                // Replace with smaller exponent to avoid overflow
+                exponentString = "1"
+                adjustedInput = adjustedInput.substring(0, indexOfE + 1) + exponentString
+            }
+
+
+            try {
+                BigDecimal(adjustedInput)
+                return true
+            } catch (e: Exception) {
+                return false
+            }
+        }
+    }
+}
