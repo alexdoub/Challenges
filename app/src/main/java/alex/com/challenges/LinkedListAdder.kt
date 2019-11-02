bac2fc4c3dfa4ae19b4dd9e50db0b17e738cadba
Alex Doub <alexdoub1990@gmail.com>
2019-11-02 11:27:47 -0700

    linked list adder

---
+++ app/src/main/java/alex/com/challenges/LinkedListAdder.kt
@@ -0,0 +1,57 @@
+package alex.com.challenges
+
+class LinkedListAdder {
+
+    class ListNode(var `val`: Int) {
+        var next: ListNode? = null
+
+        fun toProperString(): String {
+            var item: ListNode? = this
+            var printString = ""
+            while (item != null) {
+                printString += item.`val`
+                item = item.next
+            }
+
+            return printString.reversed()
+        }
+    }
+
+    companion object {
+
+        fun addTwoNumbers(l1: ListNode?, l2: ListNode?): ListNode? {
+
+            var left: ListNode? = l1!!
+            var right: ListNode? = l2!!
+            var head: ListNode? = null
+            var created: ListNode? = null
+            var carry = false
+
+            while (left != null || right != null || carry) {
+
+                // Calculate new value
+                var thisVal = (left?.`val` ?: 0) + (right?.`val` ?: 0) + if (carry) 1 else 0
+
+                // handle carry
+                carry = thisVal >= 10
+                if (thisVal >= 10) thisVal -= 10
+
+
+                // Create node and mark head OR link to previous
+                val newNode = ListNode(thisVal)
+                if (head == null) {
+                    head = newNode
+                }
+                created?.next = newNode
+
+                // Advance
+                created = newNode
+                left = left?.next
+                right = right?.next
+            }
+
+            return head
+        }
+    }
+
+}
