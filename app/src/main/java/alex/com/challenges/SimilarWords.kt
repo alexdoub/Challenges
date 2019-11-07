59bb75cc9929d51ab4370f82eb144cc8b452a423
Alex Doub <alexdoub1990@gmail.com>
2019-11-07 15:08:50 -0800

    added better solution to SimilarWords & Test

--- app/src/main/java/alex/com/challenges/SimilarWords.kt
+++ app/src/main/java/alex/com/challenges/SimilarWords.kt
@@ -1,10 +1,20 @@
 package alex.com.challenges
 
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
+
 class SimilarWords {
 
     companion object {
         fun solve(word: String, sentence: String): Int {
             return solve(word, sentence.split(" "))
+        }
+        fun solve2(word: String, sentence: String): Int {
+            return solve2(word, sentence.split(" "))
+        }
+
+        fun solveExact(word: String, sentence: String): Int {
+            return solveExact(word, sentence.split(" "))
         }
 
         private fun solve(word: String, wordList: List<String>): Int {
@@ -31,6 +41,34 @@ class SimilarWords {
                 }
             }
             return matching.size
+        }
+
+        private fun solve2(word: String, wordList: List<String>): Int {
+            val matching = ArrayList<String>().toMutableSet()
+
+            val targetWord = word.associateByTo(HashMap(), { it })
+            for (item in wordList) {
+                val thisWord = item.associateByTo(HashMap(), { it })
+
+                if (targetWord.keys == thisWord.keys) {
+                    matching.add(item)
+                }
+            }
+            return matching.size
+        }
+
+        // Must have same characters & same character count
+        private fun solveExact(word: String, wordList: List<String>): Int {
+            val matchingList = ArrayList<String>().toMutableSet()
+
+            val targetWordMap = word.groupBy { it }
+            for (item in wordList) {
+                val thisWordMap = item.groupBy { it }
+                if (targetWordMap == thisWordMap) {
+                    matchingList.add(item)
+                }
+            }
+            return matchingList.size
         }
     }
 }
--- app/src/test/java/alex/com/challenges/SimilarWordsTest.kt
+++ app/src/test/java/alex/com/challenges/SimilarWordsTest.kt
@@ -1,14 +1,25 @@
 package alex.com.challenges
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class SimilarWordsTest {
     @Test
-    fun test1() {
+    fun test_similar() {
         val word = "love"
         val wordList = "velo low vole lovee volvell lowly lower lover levo loved love lovee lowe lowes lovey lowan lowa evolve loves volvelle lowed love"
 
-        val count = SimilarWords.solve(word, wordList)
+        val count = SimilarWords.solve2(word, wordList)
         assert(count == 8)
+    }
+
+    @Test
+    fun test_exact() {
+        val word = "love"
+        val wordList = "velo low vole lovee volvell lowly lower lover levo loved love lovee lowe lowes lovey lowan lowa evolve loves volvelle lowed love"
+
+        //velo vole love levo
+        val count = SimilarWords.solveExact(word, wordList)
+        assertEquals(4, count)
     }
 }
