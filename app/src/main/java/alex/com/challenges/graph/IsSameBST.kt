51ca53fa86575bab71b4cd4856dbf499c3cd7cda
Alex Doub <alexdoub1990@gmail.com>
2020-03-31 19:35:04 -0700

    cleanup

--- app/src/main/java/alex/com/challenges/graph/IsSameBST.kt
+++ app/src/main/java/alex/com/challenges/graph/IsSameBST.kt
@@ -10,8 +10,7 @@ import alex.com.challenges.common.TreeNode
 object IsSameBST {
     fun isSameTree(p: TreeNode?, q: TreeNode?): Boolean {
         return if (p == null && q == null) true
-        else if (p?.`val` == q?.`val`) {
-            isSameTree(p?.left, q?.left) && isSameTree(p?.right, q?.right)
-        } else false
+        else if (p?.`val` == q?.`val`) isSameTree(p?.left, q?.left) && isSameTree(p?.right, q?.right)
+        else false
     }
 }
